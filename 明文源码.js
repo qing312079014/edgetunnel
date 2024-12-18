f15b9fc26d49d39450dca5846b0d00f063766827
qing312079014 <155431333+qing312079014@users.noreply.github.com>
2024-12-18 14:34:46 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,14 +1,14 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '73717741-692a-4bf4-8301-ac3cad71c136';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
 let subProtocol = 'https';
 let subEmoji = 'true';
-let socks5Address = '';
+let socks5Address = 'socks://qing3120:2858970@qing312.serv00.net:11463';
 let parsedSocks5Address = {}; 
 let enableSocks = false;
 
